1b2d0ee6482fd60ea44b6067a4dc573d810bd34d
Oscar Ruiz <oscarruiz2614@gmail.com>
2024-06-07 23:07:43 -0500

    :tada: first commit

---
+++ app/Models/EgresoFijo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class EgresoFijo extends Model
+{
+    use HasFactory;
+}
